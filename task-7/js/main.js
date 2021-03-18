f01de0ea0a49ef1c1c9f014e6aeddc1a99273572
SabineZilde <sabine.zilde@protonmail.ch>
2021-03-18 21:25:18 +0200

    Update main.js

---
+++ task-7/js/main.js
@@ -0,0 +1,10 @@
+// 7. Write a program to find the factorial of a number. 
+// Sample output:
+// Input a number to find the factorial: 5
+// The factorial of the given number is: 120
+
+const number = 5;
+
+for (let i = 1; i <= 5; i++) {
+    
+}
